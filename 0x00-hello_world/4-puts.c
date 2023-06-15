bf2aa037d145692d3ce35ea38e78a6b687a4b16e
doomysun <dumisanimavunga@gmail.com>
2023-06-15 23:56:12 +0400

    this was really hard

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this is the begining of my progran
+*Return:  will give a zero when successful
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
