2c6ace201d29cfcdad8f42770d02b061721fc18c
doomysun <dumisanimavunga@gmail.com>
2023-06-29 10:29:00 +0400

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int d = 0, n = 0;
+
+	while (*(dest + d) != '\0')
+		d++;
+
+	while (*(src + n) != '\0' && d < 97)
+	{
+		*(dest + d) = *(src + n);
+		d++;
+		n++;
+	}
+	*(dest + d) = '\0';
+	return (dest);
+}
